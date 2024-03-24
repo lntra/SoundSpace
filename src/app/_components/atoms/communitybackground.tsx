b292edc930cb04f48dfd54ad41c162149442bd64
lntra <fernandoaraujo1202@gmail.com>
2024-03-24 19:28:07 -0300

    erro de compilação

--- src/app/_components/atoms/communitybackground.tsx
+++ src/app/_components/atoms/communitybackground.tsx
@@ -1,4 +1,4 @@
-const CommunityBackground = (props : any) => {
+const CommunityBackground = () => {
     return <>
         <div className=" min-h-full	p-3 bg-indigo-50 rounded-[20px] border border-fuchsia-700 flex flex-col items-start justify-centers"></div>
     </>
--- src/app/_components/atoms/textArticle.tsx
+++ src/app/_components/atoms/textArticle.tsx
@@ -1,5 +1,5 @@
 const TextArticle = () => {
-    return <>
+    return (
         <div className="grid grid-cols-12 pt-[18px] mb-24">
            <span className="col-span-2"></span>
            <p className="col-span-8 text-gray-900 text-2xl font-semibold font-['Lato'] text-left">
@@ -24,7 +24,7 @@ const TextArticle = () => {
            </p>
            <span className="col-span-2"></span>
         </div>
-    </>
+    );
 }
 
 export default TextArticle
