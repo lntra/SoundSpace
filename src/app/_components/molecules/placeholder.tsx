be1f12b4cd6c774042d8f8d341ea1ae2f3d61360
lntra <fernandoaraujo1202@gmail.com>
2024-03-24 19:32:06 -0300

    alterando classe background

--- src/app/_components/molecules/placeholder.tsx
+++ src/app/_components/molecules/placeholder.tsx
@@ -1,14 +1,13 @@
 import Button from "../atoms/button"
 import Barlist from "../atoms/barlist"
-import CommunityBackground from "../atoms/communitybackground"
 
 const PlaceholderComunidade = () =>{
     return <>
-        <CommunityBackground>
+        <div className=" min-h-full	p-3 bg-indigo-50 rounded-[20px] border border-fuchsia-700 flex flex-col items-start justify-centers">
             <div className="text-gray-900 text-xl font-bold font-['Lato']">Destaques da Semana</div>
             <Barlist></Barlist>
             <Button></Button>
-        </CommunityBackground>
+        </div>
     </>
 }
 
