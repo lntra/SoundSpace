9dbee2d7bc8ba72fdbb865fb17a1836bb8ecd3c3
lntra <fernandoaraujo1202@gmail.com>
2024-11-04 15:03:37 -0300

    trying to fix posts

--- src/app/_components/organisms/communityPosts.tsx
+++ src/app/_components/organisms/communityPosts.tsx
@@ -55,14 +55,13 @@ const CommunityPostsSection: React.FC<CommunityPostsSectionProps> = ({
         </div>
         <div className="col-start-3 row-start-1 row-end-1"></div>
         <div className="col-span-10 row-start-2 row-end-2 grid grid-cols-10 grid-rows-1">
-          <div className="sticky top-[20%] col-start-1 col-end-4 mb-3 hidden justify-center self-start lg:col-start-1 lg:col-end-3 lg:block">
-            <CommunityLeftGuide
+        
+        <CommunityLeftGuide
               dark={dark}
               forYouRoute={forYouRoute}
               setForYou={setForYouRoute}
               userId={data?.user.id as UUID}
             ></CommunityLeftGuide>
-          </div>
           <div className="col-start-1 col-end-12 sm:col-start-1 sm:col-end-12 md:col-start-2 md:col-end-10 lg:col-start-3 lg:col-end-9">
             <Posts
               dark={dark}
